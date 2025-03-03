28664b31b2002a8a8519752a7f51190119c627eb
XUE YANG <xuey648@gmail.com>
2025-03-03 17:57:11 +0100

    Juint

--- Maven/web-project01/maven-project01/src/test/java/com/javaweb/UserServiceTest.java
+++ Maven/web-project01/maven-project01/src/test/java/com/javaweb/UserServiceTest.java
@@ -1,12 +1,31 @@
 package com.javaweb;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserServiceTest {
     @Test
     public void testGetAge() {
-        Integer age = new UserService().getAge("110002200505091218");
+        Integer age = new UserService().getAge("110002200105091218");
         System.out.println(age);
 
+    }
+
+    /**
+     * assert
+     */
+    @Test
+    public void testGenderWithAssert(){
+        UserService userService = new UserService();
+        String gender = userService.getGender("110002200505091211");
+        Assertions.assertEquals("男",gender);
+    }
+    @Test
+    public void testGenderWithAssert2(){
+        UserService userService = new UserService();
+        String gender = userService.getGender("110002200505091211");
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            userService.getGender("null");
+        });
     }
 }
