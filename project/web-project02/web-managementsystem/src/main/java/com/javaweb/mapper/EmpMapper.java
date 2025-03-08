4eac3e4cb57ce8e86a599677397b5ba0f9843400
XUE YANG <xuey648@gmail.com>
2025-03-08 02:14:25 +0100

    Requêtes paginées par PageHelper

--- project/web-project02/web-managementsystem/src/main/java/com/javaweb/mapper/EmpMapper.java
+++ project/web-project02/web-managementsystem/src/main/java/com/javaweb/mapper/EmpMapper.java
@@ -11,13 +11,18 @@ import java.util.List;
  */
 @Mapper
 public interface EmpMapper {
+    /*************************Requêtes paginées de la manière originale*************************************/
     /**
      * Interroger le nombre total d’enregistrements
      */
-    @Select("select count(*) from emp e left join dept d on e.dept_id = d.id;")
+   /* @Select("select count(*) from emp e left join dept d on e.dept_id = d.id;")
     public Long count();
 
     @Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id " +
             "order by e.update_time desc limit #{start},#{pageSize}")
-    public List<Emp> list(Integer start,Integer pageSize);
+    public List<Emp> list(Integer start,Integer pageSize);*/
+
+    @Select("select e.*, d.name deptName from emp e left join dept d on e.dept_id = d.id " +
+            "order by e.update_time desc")
+    public List<Emp> list();
 }
--- project/web-project02/web-managementsystem/src/main/java/com/javaweb/service/impl/EmpServiceImpl.java
+++ project/web-project02/web-managementsystem/src/main/java/com/javaweb/service/impl/EmpServiceImpl.java
@@ -1,9 +1,12 @@
 package com.javaweb.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.javaweb.mapper.EmpMapper;
 import com.javaweb.pojo.Emp;
 import com.javaweb.pojo.PageResult;
 import com.javaweb.service.EmpService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,13 +15,11 @@ import java.util.List;
 @Service
 public class EmpServiceImpl implements EmpService {
 
-    private final EmpMapper empMapper;
+    @Autowired
+    private EmpMapper empMapper;
 
-    public EmpServiceImpl(EmpMapper empMapper) {
-        this.empMapper = empMapper;
-    }
-
-    @Override
+/************************Requêtes paginées de la manière originale**************************************/
+    /*@Override
     public PageResult<Emp> page(Integer page, Integer pageSize) {
         //1. appel interface mapper pour interroger la nombre d'enregistrements
         Long total = empMapper.count();
@@ -27,5 +28,27 @@ public class EmpServiceImpl implements EmpService {
         List<Emp> rows = empMapper.list(start, pageSize);
         //3. encapsuler le résultat pageResult
         return new PageResult<>(total, rows);
+    }*/
+
+    /***
+     * PageHeler Requêtes paginées
+     * @param page
+     * @param pageSize
+     * @return
+     * attention!:
+     *          1. pas de ; à la fin de SQL requête
+     *          2. PageHelper traite que la première instruction de requête suivante
+     */
+    @Override
+    public PageResult<Emp> page(Integer page, Integer pageSize) {
+        //1. Définir les paramètres de pagination (PageHelper)
+        PageHelper.startPage(page, pageSize);
+
+        //2. Interroger
+        List<Emp> empList = empMapper.list();
+
+        //3. encapsuler le résultat pageResult
+        Page<Emp> p = (Page<Emp>) empList;
+        return new PageResult<Emp>(p.getTotal(), p.getResult());
     }
 }
