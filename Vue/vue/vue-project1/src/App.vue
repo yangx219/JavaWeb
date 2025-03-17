263f278ee61423660b27039693f3191abd4c2f8d
XUE YANG <xuey648@gmail.com>
2025-03-18 00:16:49 +0100

    Composable APIs

--- Vue/vue/vue-project1/src/App.vue
+++ Vue/vue/vue-project1/src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import ApiDemo from './views/ApiDemo.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ApiDemo></ApiDemo>
+  
 </template>
 
 <style scoped>
---
+++ Vue/vue/vue-project1/src/views/ApiDemo.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+//introduire ref
+import {onMounted, ref} from 'vue';
+
+//declare response data
+const count = ref(0);
+
+//declare fonction
+function increment (){
+    count.value++;
+}
+
+//Declare hook function
+onMounted(() =>{
+    console.log('Vue mounted ...')
+})
+
+
+</script>
+
+<template>
+    <button @click="increment">
+        count : {{ count }}
+    </button>
+</template>
+
+<style scoped>
+</style>
