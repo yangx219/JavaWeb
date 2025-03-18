4e49a328d6c961bcec250d5868c85274e997f7ef
XUE YANG <xuey648@gmail.com>
2025-03-18 18:20:11 +0100

    vue: Element-plus button table pagibation

--- Vue/vue/vue-project2/src/views/ElementDemo.vue
+++ Vue/vue/vue-project2/src/views/ElementDemo.vue
@@ -1,10 +1,49 @@
 <script setup>
+import { ref } from 'vue'
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
 
 
+const currentPage4 = ref(1)
 
+const pageSize4 = ref(10)
+const total = ref(400)
+const size = ref('default')
+const background = ref(true)
+const disabled = ref(false)
+
+const handleSizeChange = (val) => {
+  console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val) => {
+  console.log(`current page: ${val}`)
+}
 </script>
 
 <template>
+
+    <!-- 按钮button -->
   <div class="mb-4">
     <el-button>Default</el-button>
     <el-button type="primary">Primary</el-button>
@@ -14,15 +53,36 @@
     <el-button type="danger">Danger</el-button>
   </div>
 
+  <br>
+  
   <div class="mb-4">
-    <el-button plain>Plain</el-button>
-    <el-button type="primary" plain>Primary</el-button>
-    <el-button type="success" plain>Success</el-button>
-    <el-button type="info" plain>Info</el-button>
-    <el-button type="warning" plain>Warning</el-button>
-    <el-button type="danger" plain>Danger</el-button>
+    <!-- 表格 table -->
+
+  <el-table :data="tableData" border style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
   </div>
   
+
+  <div class="mb-4">
+    <!-- 分页条 -->
+   
+  <el-pagination
+      v-model:current-page="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[10, 20, 30, 40]"
+      :size="size"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+
 </template>
 
 <style scoped>
